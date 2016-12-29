7ef527f39798aa73dce2a49769a58d6787fde6df
liu zhaofeng <2622967271@qq.com>
2016-12-29 09:08:45 +0800

    modify OrderActionBean.java

--- MyJpetstore/src/main/java/com/langtaojin/myjpetstore/web/actions/OrderActionBean.java
+++ MyJpetstore/src/main/java/com/langtaojin/myjpetstore/web/actions/OrderActionBean.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import com.langtaojin.myjpetstore.domain.Order;
 import com.langtaojin.myjpetstore.service.OrderService;
 
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.SessionScope;
 import net.sourceforge.stripes.integration.spring.SpringBean;
 
@@ -71,7 +75,78 @@ public class OrderActionBean extends AbstractActionBean{
 		this.shippingAddressRequired = shippingAddressRequired;
 	}
 
+	public List<String> getCreditCardTypes(){
+		return CARD_TYPE_LIST;
+	}
 	
+	public Resolution listOrders(){
+		HttpSession session=context.getRequest().getSession();
+		AccountActionBean accountActionBean=(AccountActionBean) session.getAttribute("/actions/Account.action");
+		orderList=orderService.getOrdersByUsername(accountActionBean.getUsername());
+		return new ForwardResolution(LIST_ORDERS);
+	}
 	
+	public Resolution newOrderForm() {
+		HttpSession session=context.getRequest().getSession();
+		AccountActionBean accountActionBean=(AccountActionBean) session.getAttribute("/actions/Account.action");		
+		CartActionBean cartActionBean=(CartActionBean) session.getAttribute("/actions/Cart.action");
+		
+		clear();
+		if(accountActionBean==null || !accountActionBean.isAuthenticated()){
+			setMessage("You must sign on before attempting to check out.  Please sign on and try checking out again.");
+			return new ForwardResolution(AccountActionBean.class);
+		}else if(cartActionBean!=null){
+			order.initOrder(accountActionBean.getAccount(), cartActionBean.getCart());
+			return new ForwardResolution(NEW_ORDER);
+		}else{
+			setMessage("An order could not created because a cart could not be found");
+			return new ForwardResolution(ERROR);
+		}		
+	}
 	
+	public Resolution newOrder() {
+		HttpSession session=context.getRequest().getSession();
+		
+		if(shippingAddressRequired){
+			shippingAddressRequired=false;
+			return new ForwardResolution(SHIPPING);
+		}else if(!isConfirmed()){
+			 return new ForwardResolution(CONFIRM_ORDER);
+		}else if(getOrder()!=null){
+			orderService.insertOrder(order);
+			
+			CartActionBean cartActionBean=(CartActionBean) session.getAttribute("/actions/Cart.action");
+			cartActionBean.clear();
+			
+			setMessage("Thank you, your order has been submitted.");
+			return new ForwardResolution(VIEW_ORDER);
+		}else{
+		    setMessage("An error occurred processing your order (order was null).");
+		    return new ForwardResolution(ERROR);
+		}
+	}
+	
+	public Resolution viewOrder() {
+		HttpSession session=context.getRequest().getSession();
+		
+		AccountActionBean accountActionBean=(AccountActionBean) session.getAttribute("/actions/Account.action");
+		
+		order=orderService.getOrder(order.getOrderId());
+		
+		if(accountActionBean.getAccount().getUsername().equals(order.getUsername())){
+			return new ForwardResolution(VIEW_ORDER);
+		}else{
+			order=null;
+			setMessage("You may only view your own orders");
+			return new ForwardResolution(ERROR);
+		}
+	}
+	
+	public void clear() {
+		order=new Order();
+		shippingAddressRequired=false;
+		confirmed=false;
+		orderList=null;
+	}
+		
 }
